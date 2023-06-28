afcd478cf912d13d25401e3670bafb82cbf28029
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-06-28 01:22:30 -0400

    feat
    eslint prettier tailwind

---
+++ web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
+    </div>
+  )
+}
