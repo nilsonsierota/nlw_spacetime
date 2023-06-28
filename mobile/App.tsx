a53356a990a91f9f0f0573fcbfc45f63d2bfe204
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-06-28 01:41:24 -0400

    dev
    eslint tailwind prettier

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl">Hello World</Text>
+      <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
