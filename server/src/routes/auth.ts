74187574b245a0e3ded5ee6aa9a9b4a601725cd9
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-07-07 01:55:58 -0400

    feat: auth routes and memories routes
    implementation of routes with need jwt authenticated

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -60,6 +60,14 @@ export async function authRoutes(app: FastifyInstance) {
       })
     }
 
-    return { user }
+    const token = app.jwt.sign(
+      { name: user.name, avatarUrl: user.avatarUrl },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      },
+    )
+
+    return { token }
   })
 }
