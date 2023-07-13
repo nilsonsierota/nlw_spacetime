49188745d632398929cac406ac721e278219db98
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-07-13 00:11:12 -0400

    feat: Profile
    Implement the Profile component

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -13,9 +13,11 @@ export async function GET(request: NextRequest) {
 
   const redirectURL = new URL('/', request.url)
 
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/;`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   })
 }
