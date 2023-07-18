4b6fdf7e4de37ea751da2fdea86f8e4f94171163
Nilson Sierota Correia <nilsoncv2@gmail.com>
2023-07-18 00:38:41 -0400

    feat: middleware
    implement of middleware signin and redirectto.

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,13 +1,13 @@
-export function EmptyMemories(){
+export function EmptyMemories() {
   return (
     <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
-            Voce ainda nao registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+      <p className="w-[360px] text-center leading-relaxed">
+        Voce ainda nao registrou nenhuma lembrança, comece a{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
   )
 }
